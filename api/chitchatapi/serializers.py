f7dbecbb3639003e411490f40a53df1ecb221556
Miguel <rawredonion@gmail.com>
2021-11-24 20:50:38 -0500

    Added comments for future implementation.

--- api/chitchatapi/serializers.py
+++ api/chitchatapi/serializers.py
@@ -1,12 +1,16 @@
 from chitchatapi.models import User, Chat
 from rest_framework import serializers
+
+
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = ("__all__")
 
+
 class UserSerializer(serializers.ModelSerializer):
     chats = ChatSerializer(many=True).data
+
     class Meta:
         model = User
         fields = ("__all__")
--- api/chitchatapi/views.py
+++ api/chitchatapi/views.py
@@ -6,20 +6,42 @@ from rest_framework.response import Response
 from chitchatapi.models import User
 from .serializers import UserSerializer, ChatSerializer
 
+
 class ListUsers(APIView):
-    def get(self, reqeuest, format = None):
+    def get(self, reqeuest, format=None):
         users = User.objects.all()
         users = UserSerializer(users, many=True)
         return Response(users.data)
+
+
 class ListChats(APIView):
-    def get(self, reqeuest, format = None):
+    def get(self, request, format=None):
         return Response(ChatSerializer(Chat.objects.all(), many=True).data)
 
-# @api_view(["GET"])
-# def index(request):
+
+# class ListMessages(APIView):
+#     def get(self, request, format):
 
 
-# Create your views here.
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
+class ChatManage():
+    # creating a new chat
+    # deleting a chat
+    # archiving a chat
+    # creating a new message in a chat
+    # deleting a message in a chat
+    # Editing chats (could be a feature)
+    def get(self):
+        pass
 
+
+class ListNotifications():
+    # getting notifications (once user reads, delete)
+
+    # class ListArchive(APIView):
+
+    # @api_view(["GET"])
+    # def index(request):
+
+    # Create your views here.
+    # class UserList(generics.ListAPIView):
+    #     queryset = User.objects.all()
