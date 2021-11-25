335a72267af10cec20878da6d6e345f6746a51b2
AJ Heflin <me@ajheflin.com>
2021-11-24 22:54:36 -0500

    now it works

--- api/api/urls.py
+++ api/api/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import re_path
 from chitchatapi import views
+#hello
 urlpatterns = [
     #admin/
     path('admin/', admin.site.urls),
