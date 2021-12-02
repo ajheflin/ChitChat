452a429431da8f69b7a06deb7d9b0ae2b392886b
Miguel <rawredonion@gmail.com>
2021-12-01 22:41:31 -0500

    Create template for Settings.

---
+++ client/src/components/SettingsOptions.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <div>
+      <h3 class="text-xl text-gray-600">Change Name:</h3>
+      <v-text-field
+        v-model="newName"
+        label="Name"
+        hide-details="auto"
+      ></v-text-field>
+      <v-btn class="mt-5" color="primary" @click="changeName"
+        >Change Name</v-btn
+      >
+    </div>
+    <!-- <div>
+      <h3 class="text-xl text-gray-500">Change :</h3>
+      <v-text-field
+        v-model="newName"
+        label="Name"
+        hide-details="auto"
+      ></v-text-field>
+    </div> -->
+    <!-- v-btn -->
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import axios from "axios";
+
+@Component({})
+export default class SettingsOptions extends Vue {
+  public newName = "";
+
+  public changeName() {
+    // TODO implement axios requests on each change function
+    console.log(`Name has been change to ${this.newName}`);
+  }
+}
+</script>
