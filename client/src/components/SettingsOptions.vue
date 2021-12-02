015c132df8aabaed9c15672aab3c7b5086f19e53
Winston Bass <winstonabass@gmail.com>
2021-12-01 23:49:38 -0500

    Co-authored-by: AJ Heflin <ajheflin@users.noreply.github.com>

--- client/src/components/SettingsOptions.vue
+++ client/src/components/SettingsOptions.vue
@@ -7,19 +7,75 @@
         label="Name"
         hide-details="auto"
       ></v-text-field>
-      <v-btn class="mt-5" color="primary" @click="changeName"
-        >Change Name</v-btn
-      >
+      <v-btn class="mt-5" color="primary" @click="changeName">
+        Change Name
+      </v-btn>
     </div>
-    <!-- <div>
-      <h3 class="text-xl text-gray-500">Change :</h3>
+
+    <div>
+      <h3 class="text-xl text-gray-500">Change Username:</h3>
       <v-text-field
-        v-model="newName"
-        label="Name"
+        v-model="newUsername"
+        label="Username"
         hide-details="auto"
       ></v-text-field>
-    </div> -->
-    <!-- v-btn -->
+    </div>
+    <v-btn class="mt-5" color="primary" @click="changeUN">
+      Change Username
+    </v-btn>
+
+    <div>
+      <h3 class="text-xl text-gray-500">Change Password:</h3>
+      <v-text-field
+        v-model="currentPassword"
+        label="Current Password"
+        hide-details="auto"
+      ></v-text-field>
+      <v-text-field
+        v-model="newPassword"
+        label="New Password"
+        hide-details="auto"
+      ></v-text-field>
+      <v-text-field
+        v-model="confirmNew"
+        label="Confirm New Password"
+        hide-details="auto"
+      ></v-text-field>
+    </div>
+    <v-btn class="mt-5" color="primary" @click="changePass">
+      Change Password
+    </v-btn>
+
+    <div>
+      <h3 class="text-xl text-gray-500">Delete Account</h3>
+    </div>
+
+    <div>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="red" dark v-bind="attrs" v-on="on"> Delete </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Delete Account?
+          </v-card-title>
+
+          <v-card-text>
+            Are you sure you want to delete your account?
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="deleteAccount">
+              Yes, I'm Sure
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </v-container>
 </template>
 
@@ -31,10 +87,42 @@ import axios from "axios";
 @Component({})
 export default class SettingsOptions extends Vue {
   public newName = "";
+  public newUsername = "";
+  public newPassword = "";
+  public confirmNew = "";
+  public currentPassword = "";
+  public dialog = false;
 
-  public changeName() {
-    // TODO implement axios requests on each change function
-    console.log(`Name has been change to ${this.newName}`);
+  get user() {
+    return this.$store.getters["AuthModule/getUser"];
+  }
+
+  public async changeName() {
+    await axios.post("/api/users/changeName/", {
+      user_id: this.user.id,
+      newName: this.newName,
+    });
+  }
+
+  public async changeUN() {
+    await axios.post("/api/users/changeUsername/", {
+      user_id: this.user.id,
+      newUsername: this.newUsername,
+    });
+  }
+
+  public async changePass() {
+    if (this.newPassword == this.confirmNew) {
+      await axios.post("/api/users/changePass/", {
+        user_id: this.user.id,
+        newPassword: this.newPassword,
+      });
+    }
+  }
+
+  public async deleteAccount() {
+    await axios.get(`/api/users/delete/${this.user.id}`);
+    this.dialog = false;
   }
 }
 </script>
